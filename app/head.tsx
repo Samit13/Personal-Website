88c9c7522963b3e2c66b14b85d0f4d83f33d8c3b
Samit Madatanapalli <vsamit.palli@gmail.com>
2025-10-02 21:58:37 -0400

    dsajklfjdsklfasd

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" type="image/png" sizes="32x32" href="/websiteicon.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/websiteicon.png" />
+      <link rel="apple-touch-icon" href="/websiteicon.png" />
+    </>
+  )
+}
